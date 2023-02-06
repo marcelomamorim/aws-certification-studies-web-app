db655a875e09cb6f5cb5ad8f553e78b97fdb80ed
Marcelo <marcelo.med.amorim@gmail.com>
2023-02-05 23:42:26 -0300

    inicializacao do projeto

---
+++ estudos-certificacao-aws/estudos-certificacao-aws/src/main/java/com/br/estudoscertificacaoaws/EstudosCertificacaoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.br.estudoscertificacaoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosCertificacaoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosCertificacaoAwsApplication.class, args);
+	}
+
+}
